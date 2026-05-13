854398db7b5d6044bbd8b8d06917e5402faafc2a
nzolliker <nicola.zolliker@hotmail.com>
2026-05-13 11:28:46 +0200

    harpoon, markdown-preview, blink update

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,11 @@
+return {
+	"saghen/blink.cmp",
+	version = "1.*",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+	},
+	build = "cargo build --release",
+	opts = {
+		-- your existing config
+	},
+}
